b5dc177929617d7287087c392ea9bc208eeb931a
yashmanian <ymanian@umd.edu>
2018-02-21 18:55:17 -0500

    Added initial package for qualifier 1

---
+++ pnp_qualifier/src/test_node.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <vector>
+
+#include <ros/ros.h>
+
+#include <osrf_gear/LogicalCameraImage.h>
+#include <osrf_gear/Order.h>
+#include <osrf_gear/Proximity.h>
+#include <sensor_msgs/JointState.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/Range.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/String.h>
+#include <std_srvs/Trigger.h>
+//#include <osrf_gear/VaccumGripperControl.h>
+#include <trajectory_msgs/JointTrajectory.h>
+
+
+/// Start the competition by waiting for and then calling the start ROS Service.
+void start_competition(ros::NodeHandle & node) {
+  // Create a Service client for the correct service, i.e. '/ariac/start_competition'.
+  ros::ServiceClient start_client =
+    node.serviceClient<std_srvs::Trigger>("/ariac/start_competition");
+  // If it's not already ready, wait for it to be ready.
+  // Calling the Service using the client before the server is ready would fail.
+  if (!start_client.exists()) {
+    ROS_INFO("Waiting for the competition to be ready...");
+    start_client.waitForExistence();
+    ROS_INFO("Competition is now ready.");
+  }
+  ROS_INFO("Requesting competition start...");
+  std_srvs::Trigger srv;  // Combination of the "request" and the "response".
+  start_client.call(srv);  // Call the start Service.
+  if (!srv.response.success) {  // If not successful, print out why.
+    ROS_ERROR_STREAM("Failed to start the competition: " << srv.response.message);
+  } else {
+    ROS_INFO("Competition started!");
+  }
+}
+
+
+int main(int argv, char ** argc)
+{
+	// Initialize node
+	ros::init(argc, argv, "test_node");
+
+	ros::NodeHandle node;
+}
